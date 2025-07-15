3397c6f759820f28702a8e0d74c7622a01d0159e
pranavasati28 <120238503+pranavasati28@users.noreply.github.com>
2025-07-15 05:59:18 +0000

    math operations

--- 01_basics/operations.js
+++ 01_basics/operations.js
@@ -1,1 +1,6 @@
-// Operations
+// math operations 
+console.log(Math.random);
+const min = 10;
+const max = 50;
+console.log(Math.floor(Math.random() * (max - min + 1 )) + min)
+// refers to number and math video if forgot this logic
